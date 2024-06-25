b08a8fcb5322b93d1c69ea32c69b1688660ad8b3
13392 <134060455+13392@users.noreply.github.com>
2024-06-25 12:59:03 -0300

    Páginas do Site

---
+++ paginas/home.php
@@ -0,0 +1,67 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<div class="container">
+    <h1 class="text-center">
+        Principais Novos Laçamentos
+    </h1> 
+</div>
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+
+    <div class="carousel-item active conteiner col-12">
+      <img src="./imagens/banner1.png" class="d-block w-100" alt="manha">
+    </div>
+    <div class="carousel-item">
+      <img src="./imagens/banner2.jpg" class="d-block w-100" alt="tarde">
+    </div>
+    <div class="carousel-item">
+      <img src="./imagens/banner3.jpg" class="d-block w-100" alt="noite">
+    </div>
+  </div>
+  <button class="carousel-control-prev bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button> 
+</div>
+<h2 class="mk-3 conteiner text-center col-12">Jogos </h2>
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
---
+++ paginas/suporte.php
@@ -0,0 +1,20 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<section class="form" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+  <form name="formContato" action="" method="post">
+
+    <input type="hidden" name="accessKey" vaLue="df8f0503-63c6-41fd-a53f-595c9a3b3808"><!--chave para enviar o email-->
+
+    <label data-aos="fade-left">Nome</label>
+    <input type="text" name="name" required placeholder="Nome completo" autocomplete="off">
+
+    <label data-aos="fade-left">Email</label>
+    <input type="email" name="email" placeholder="Digite seu Email" autocomplete="off">
+
+    <label data-aos="fade-left">Mensagem</label>
+    <textarea name="Digite sua Mensagem"></textarea>
+
+    <button data-aos="fade-left" class="button" type="submit">Enviar</button>
+
+  </form>
+</section>
